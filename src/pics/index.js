bfdd7cb91e535bc60bc53d132a0f500b947d2d54
Zee <110691931+ZahraHS7@users.noreply.github.com>
2023-08-07 22:35:54 +0330

    Add pictures of the web

---
+++ src/pics/index.js
@@ -0,0 +1,7 @@
+// pics/index.js
+import film from './film.jpg';
+import watcingTV from './watching TV.jpg';
+import popcornFilm from './popcornFilm.jpg';
+import logo from './logo.png';
+
+export { film, watcingTV, popcornFilm, logo };
