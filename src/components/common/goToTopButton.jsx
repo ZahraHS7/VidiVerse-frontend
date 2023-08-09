8fc0dd90c5dd1e01c84caa5589509d94c8ce5ce2
Zee HS <zahrahyt.7@gmail.com>
2023-08-09 16:40:55 +0330

    Seperate goToTopButton css code

--- src/components/common/goToTopButton.jsx
+++ src/components/common/goToTopButton.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { FaArrowCircleUp } from 'react-icons/fa';
+import '../../css files/goToTopButton.css'
 
 const GoToTopButton = () => {
   const [showButton, setShowButton] = useState(false);
