fac8345a3d70f21ac205c5f04d6d24896cf81e25
Zee HS <zahrahyt.7@gmail.com>
2023-08-15 00:11:12 +0330

    Added new pics for sidebar and not-found

--- src/pics/index.js
+++ src/pics/index.js
@@ -1,7 +1,9 @@
 // pics/index.js
 import film from './film.jpg';
+import notFound from './404 error.png';
+import cactus from './cactus_ desert.png';
 import watcingTV from './watching TV.jpg';
 import popcornFilm from './popcornFilm.jpg';
 import logo from './logo.png';
 
-export { film, watcingTV, popcornFilm, logo };
+export { film, notFound, cactus, watcingTV, popcornFilm, logo };
